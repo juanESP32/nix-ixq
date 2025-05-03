30fe28f09efdd1758926b72c15c17ff003c9f74f
juanESP32 <juanmanuelverza2@gmail.com>
2025-05-03 00:05:43 -0300

    Realizando prueba manual MQTT

--- Server/server.js
+++ Server/server.js
@@ -123,7 +123,23 @@ app.get("/payment-status", (req, res) => {
     paymentConfirmed: !!lastPaymentId,
   });
 });
+/////////////////
+app.get("/test-mqtt", (req, res) => {
+  const testPayload = {
+    precio: 99 // Elegí cualquier valor para probar
+  };
 
+  mqttClient.publish("expendedora/snacko/venta", JSON.stringify(testPayload), { qos: 1 }, err => {
+    if (err) {
+      console.error("❌ Error al publicar test MQTT:", err);
+      res.status(500).send("Error publicando MQTT");
+    } else {
+      console.log("📤 Mensaje MQTT de prueba publicado:", testPayload);
+      res.send("Mensaje MQTT de prueba enviado correctamente");
+    }
+  });
+});
+////////////
 app.listen(8080, "0.0.0.0", () => {
   console.log("Servidor corriendo en http://0.0.0.0:8080");
 });
